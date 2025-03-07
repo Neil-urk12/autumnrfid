4a6239dae8a1cc6621c492214c6959313604193b
Neil Vallecer <neilvallecer12@gmail.com>
2025-03-08 02:06:43 +0800

    feat: add Server-Sent Events endpoint and update home page for real-time student information

--- internal/handlers/card_scan_handler.go
+++ internal/handlers/card_scan_handler.go
@@ -13,14 +13,9 @@ func (h *AppHandler) HandleCardScan(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusBadRequest).SendString("RFID is required")
 	}
 
-	fmt.Println("RFID:", rfid)
-
-	// Create a new RFIDRepository instance
 	rfidRepo := repositories.NewRFIDRepository(h.db)
-
 	student, err := rfidRepo.GetStudentByRFID(rfid)
 	if err != nil {
-		fmt.Println(err)
 		return ctx.Status(fiber.StatusInternalServerError).SendString("Database error")
 	}
 
@@ -37,8 +32,37 @@ func (h *AppHandler) HandleCardScan(ctx *fiber.Ctx) error {
 
 	// Pass data to the template
 	return ctx.Render("home", fiber.Map{
-		"Student": student,
-		// "Grades":  grades,
-		// "Bills":   bills,
+		"Student":   student,
+		"Title":     "Student Information",
+		"YearLevel": getYearLevelString(student.YearLevel),
 	})
 }
+
+func getYearLevelString(year int) string {
+	switch year {
+	case 1:
+		return "First"
+	case 2:
+		return "Second"
+	case 3:
+		return "Third"
+	case 4:
+		return "Fourth"
+	default:
+		return fmt.Sprintf("%dth", year)
+	}
+}
+func (h *AppHandler) HandleSSE(c *fiber.Ctx) error {
+	c.Set("Content-Type", "text/event-stream")
+	c.Set("Cache-Control", "no-cache")
+	c.Set("Connection", "keep-alive")
+	c.Set("Transfer-Encoding", "chunked")
+
+	return c.SendString("data: connected\n\n")
+}
+
+// func (h *AppHandler) HandleSSEEvent(c *fiber.Ctx) error {
+// 	rfid := c.FormValue("rfid")
+// 	if rfid == "" {
+// 		return c.Status(fiber.StatusBadRequest).SendString("RFID is required")
+// 	}
