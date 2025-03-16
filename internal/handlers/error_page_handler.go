1e70c2db5cb76b8f349e13b31bb11cb89ed64bfd
Neil Vallecer <neilvallecer12@gmail.com>
2025-03-16 22:22:07 +0800

    feat: create custom error page with 404 message and styling

--- internal/handlers/error_page_handler.go
+++ internal/handlers/error_page_handler.go
@@ -3,5 +3,7 @@ package handlers
 import "github.com/gofiber/fiber/v2"
 
 func (h *AppHandler) HandleError(c *fiber.Ctx) error {
-	return c.Render("partials/error", fiber.Map{})
+	return c.Render("pages/error_page", fiber.Map{
+		"Title": "Error",
+	})
 }
