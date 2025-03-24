8fa9b499e940a1382a00223f9b9d639d607bc3fc
Neil Vallecer <neilvallecer12@gmail.com>
2025-03-24 11:57:19 +0800

    feat: structured the demo version of the bills_handler and grades_handler
    - created the handler for retrieving the student's grades and bills
    - created the functions for querying the database with student's rfid

--- internal/handlers/grades_handler.go
+++ internal/handlers/grades_handler.go
@@ -2,19 +2,39 @@ package handlers
 
 import (
 	"fmt"
+	"rfidsystem/internal/repositories"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func (h *AppHandler) HandleGrades(ctx *fiber.Ctx) error {
-	err := ctx.Render("pages/grades", fiber.Map{
-		"Title": "Student Grades",
-	})
-	if err != nil {
-		fmt.Printf("Template rendering error: %v\n", err)
-		return err
+	studentId := ctx.Query("student-id")
+
+	if studentId == "" {
+		return ctx.Status(fiber.StatusBadRequest).SendString("Student Id is required")
 	}
-	return nil
+
+	gradesRepo := repositories.NewRFIDRepository(h.db)
+	gradesData, err := gradesRepo.GetStudentGradesByRFID(studentId)
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).SendString("Internal server error")
+	}
+	if gradesData == nil {
+		return ctx.Status(fiber.StatusNotFound).SendString("Student not found")
+	}
+
+	// err := ctx.Render("pages/grades", fiber.Map{
+	// 	"Title": "Student Grades",
+	// })
+	return ctx.Render("grades", fiber.Map{
+		"Title":  "Student Grades",
+		"Grades": gradesData,
+	})
+
+	// if err != nil {
+	// 	fmt.Printf("Template rendering error: %v\n", err)
+	// 	return err
+	// }
 }
 
 func (h *AppHandler) HandleTestGrades(ctx *fiber.Ctx) error {
