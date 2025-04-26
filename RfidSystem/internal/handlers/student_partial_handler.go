0857eb751b565a9406ef05a03ade00ea508a7197
Neil-urk12 <neilvallecer12@gmail.com>
2025-04-26 19:59:28 +0800

    chore: remove unsused code

--- RfidSystem/internal/handlers/student_partial_handler.go
+++ RfidSystem/internal/handlers/student_partial_handler.go
@@ -128,21 +128,6 @@ func (h *AppHandler) HandleStudentInfo(ctx *fiber.Ctx) error {
 	})
 }
 
-func (h *AppHandler) GetStudentPartial(ctx *fiber.Ctx) error {
-	rfid := ctx.Params("rfid")
-
-	rfidRepo := repositories.NewRFIDRepository(h.db)
-	student, err := rfidRepo.GetStudentByRFID(rfid)
-
-	if err != nil || student == nil {
-		return ctx.Render("error_page", fiber.Map{})
-	}
-
-	return ctx.Render("partials/student_info", fiber.Map{
-		"Student": student,
-	}, "")
-}
-
 func (h *AppHandler) HandleStudentPartial(c *fiber.Ctx) error {
 	rfid := c.Params("rfid")
 
